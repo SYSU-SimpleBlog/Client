7014bab207d91a44e832d627c95701580ba6ef45
xutaiqi <869307087@qq.com>
2019-12-06 19:55:18 +0800

    delete

--- src/components/articleList.vue
+++ src/components/articleList.vue
@@ -2,13 +2,14 @@
     <div id='articlelist'>
         <h1 id='h1'>文章列表</h1>
         <ul>
-            <li id="list" v-for="item in articles">
+            <li id="list" v-for="(item,index) in articles":key="index">
                 <div id='title'>
                     <router-link :to="{
 					path:'/articleDetail/',
 					name: 'articlePage',
 					params: {id: item.id}}">{{item.name}}
                     </router-link>
+					<Icon type="ios-trash" id="icon" @click="deleteArticle(index)"/>
                 </div>
             </li>
         </ul>
@@ -58,114 +59,160 @@
 </template>
 
 <script>
-    import { GetArticles } from '../lib/api.js'
-    var currentPage = 1;
-    export default {
-        name: "articleList",
-        created() {
-            this.getData()
-            this.findArticles()
-        },
-        data() {
-            return {
-                namelist: [],
-                articles: []
-            }
-        },
-        props: {
-            currentPage: 1,
-            lowLimit: 1,
-            hiLimit: 8,
-            listSize: 10,
-            totalArticles: 100,
-            totalPages: 10,
-            loading: false,
-        },
-        methods: {
-            getData() {
-                let me = this
-                me.currentPage = 1
-                me.lowLimit = 1
-                me.hiLimit = 10
-                me.listSize = 10
-                me.totalArticles = 100
-                me.totalPages = 10
-            },
-
-            switchToPage: function (pageNo) {
-                if (pageNo <= 0 || pageNo > this.totalPages) {
-                    return;
-                }
-                this.currentPage = pageNo
-                let me = this
-                GetArticles({
-                    page: me.currentPage,
-                })
-                    .then(function (res) {
-                        me.articles = res.data.Articles
-                        // me.articles=[1,2,3,4,5,6,7,8,9,0]
-                    })
-                    .catch(function (err) {
-                        me.articles = []
-                    })
-            },
-
-            findArticles() {
-                this.currentPage = 1
-                let me = this
-                GetArticles({
-                    page: me.currentPage,
-                })
-                    .then(function (res) {
-                        me.articles = res.data.Articles
-                    })
-                    .catch(function (err) {
-                        me.articles = []
-                    })
-            }
-        }
-
-    }
-</script>
-
-<style scoped>
-    #h1 {
-        text-align: center;
-    }
-
-    #articlelist {
-        margin-top: 50px;
-        padding: 0 250px;
-    }
-
-    #list {
-        font-size: 25px;
-        list-style: none;
-        border-bottom: solid 1px #24292E;
-        margin-bottom: 2px;
-        margin-top: 2px;
-        height: 50px;
-    }
-
-    #title {
-        width: 100%;
-        text-align: left;
-        height: 100%;
-        float: left;
-        padding: 5px;
-    }
-
-    #author {
-        width: 50%;
-        text-align: right;
-        height: 100%;
-        float: left;
-        padding: 5px;
-    }
-
-    .page-item {
-        list-style: none;
-        display: inline-block;
-        text-align: center;
-    }
-</style>
+		import {GetArticles} from '../lib/api.js'
+			import {DeleteArticleById} from '../lib/api.js'
+			import axios from 'axios'
+			import qs from 'qs'
+		
+		
+			var currentPage=1;
+		    export default {
+		        name: "articleList",
+				created() {
+					this.getData()
+					this.findArticles()
+				},
+				data(){
+						return {
+							//namelist:namelist,
+							 namelist:[],
+							 articles:[]
+						}
+				},
+				props:{
+					currentPage: 1,
+					lowLimit: 1,
+					hiLimit: 8,
+					listSize: 10,
+					totalArticles: 100,
+					totalPages: 10,
+					loading: false,
+				},
+				methods:{
+					getData(){
+						let me=this
+						me.currentPage=1
+						me.lowLimit=1
+						me.hiLimit=10
+						me.listSize=10
+						me.totalArticles=100
+						me.totalPages=10
+					},
+					
+					switchToPage:function(pageNo) {
+					 if (pageNo <= 0 || pageNo > this.totalPages){
+					     return;
+					   }
+					   this.currentPage = pageNo
+					   //alert(this.currentPage)
+					   let me = this
+					   GetArticles({
+					     page: me.currentPage,
+					   })
+					   .then(function(res){
+					     me.articles = res.data.Articles
+					   })
+					   .catch(function(err) {
+					     me.articles = []
+						 alert("err:"+err)
+					   })
+					},
+					 findArticles(){
+						this.currentPage = 1
+						//alert(this.currentPage)
+						let me = this
+						GetArticles({
+						  page: me.currentPage,
+						})
+						.then(function(res){
+						  me.articles = res.data.Articles
+						  // me.articles=[1,2,3,4,5,6,7,8,9,0]
+						  me.totalPages=Math.ceil(res.data.PageCount/10)
+						})
+						.catch(function(err) {
+						  me.articles = []
+						})
+					},
+					deleteArticle(index){
+						let i=index
+						alert(this.articles[index].id)
+						DeleteArticleById({
+							id: index
+						})
+						.then(function(res){
+							
+						})
+						.catch(function(err){
+							alert(err)
+						})
+						let me = this
+							GetArticles({
+							  page: me.currentPage,
+							})
+							.then(function(res){
+							  me.articles = res.data.Articles
+							  // me.articles=[1,2,3,4,5,6,7,8,9,0]
+							  me.totalPages=Math.ceil(res.data.PageCount/10)
+							})
+							.catch(function(err) {
+							  me.articles = []
+							})
+						//alert(this.currentPage)
+					}
+					
+				}
+				
+		    }
+		</script>
+		
+		<style scoped>
+			#h1{
+				text-align: center;
+			}
+			#articlelist{
+				margin-top: 50px;
+				padding: 0 250px;
+			}
+			#list{
+				font-size: 25px;
+				list-style: none;
+				border-bottom: solid 1px #24292E ;
+				margin-bottom: 2px;
+				margin-top: 2px;
+				height: 50px;
+			}
+			#title{
+				width: 100%;
+				text-align: left;
+				height: 100%;
+				float: left;
+				padding: 5px;
+			}
+			#author{
+				width: 50%;
+				text-align: right;
+				height: 100%;
+				float: left;
+				padding: 5px;
+			}
+			#pageul{
+				
+				display: inline;
+				white-space: nowrap;
+			}
+			.page-item{
+				list-style: none;
+				display: inline-block;
+				margin: 3px;
+				margin-left: 80px;
+				text-align: center;
+				float: left;
+			}
+			#icon{
+				display: inline-block;
+				float: right;
+				margin:10px;
+			}
+		</style>
+	
+   
